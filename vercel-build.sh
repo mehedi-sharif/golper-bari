20f02a976f0e8b57cb2285733f96072c797d4710
mehedi-sharif <mehedishariftitas@gmail.com>
2025-07-07 15:02:42 +0600

    update vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # default versions
-if [ -z $GO_VERSION ]; then GO_VERSION='1.16.2'; fi
-if [ -z $HUGO_VERSION ]; then HUGO_VERSION='0.89.4'; fi
+GO_VERSION='1.24.2';
+HUGO_VERSION='0.146.5';
+
+echo "USING NODE VERSION: $(node -v)"
 
 # install Go
+echo "Installing Go $GO_VERSION..."
 curl -sSOL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
 tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+rm -rf go${GO_VERSION}.linux-amd64.tar.gz
+go version
 
 # install Hugo
 echo "Installing Hugo $HUGO_VERSION..."
 curl -sSOL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+mv hugo /usr/local/bin/
+rm -rf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 hugo version
 
-# run Hugo
-echo "Running Hugo..."
-hugo --gc --minify
+# install dependencies
+echo "Installing project dependencies..."
+npm install
+
+# run the build command
+echo "Running the build command..."
+npm run build
